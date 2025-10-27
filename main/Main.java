b5d1a5c2a85d0410eb5d9e772e91e7eb5526fca4
Ed <eduardohmdeeke@gmail.com>
2025-10-27 19:28:31 -0300

    First commit

--- main/Main.java
+++ main/Main.java
@@ -1,4 +1,7 @@
 package main;
+
+import javax.swing.JOptionPane;
+
 import calculadora.Calculadora;
 
 public class Main {
@@ -6,5 +9,35 @@ public class Main {
         String opcao = "";
         Calculadora calc = new Calculadora();
 
+        while (!opcao.equals("9")) {
+            opcao = JOptionPane.showInputDialog("1 - Leitura \\n" +
+                    "2 - Adição \\n" +
+                    "3 Subtração + \"\\n" +
+                    "4 Multiplicação n5 - Divisão \\n" +
+                    "9 Sair");
+            switch (Integer.parseInt(opcao)) {
+                case 1: {
+                    calc.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor A: ")));
+                    calc.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor B: ")));
+                    break;
+                }
+                case 2: {
+                    JOptionPane.showMessageDialog(null, "Soma: " + calc.getSoma());
+                    break;
+                }
+                case 3: {
+                    JOptionPane.showMessageDialog(null, "Diferença: " + calc.getDiferenca());
+                    break;
+                }
+                case 4: {
+                    JOptionPane.showMessageDialog(null, "Produto: " + calc.getProduto());
+                    break;
+                }
+                case 5: {
+                    JOptionPane.showMessageDialog(null, "Quociente: " + calc.getQuociente());
+                    break;
+                }
+            }
+        }
     }
 }
